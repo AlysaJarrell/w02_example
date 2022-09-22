9dee635e474d0a51d919ebc63ed6f9b0ebe94307
AlysaJarrell <cra20007@byui.edu>
2022-09-22 10:53:32 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace w02_example // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string>();
+            names.Add("Bob");
+            names.Add("Bill");
+            names.Add("Sue");
+            
+            names.ForEach(name => Console.Write(name + " "));
+
+            string aString = "a";
+            List<string> stuff = new List<string> {aString, "b", "c","d" };
+
+            char aSingleCharacter = 'e';
+            Console.WriteLine($"This is a single character: {aSingleCharacter}");
+
+            int x = 5;
+            int y = 7;
+            int result = Adder(x,y); // 12
+
+            int result2 = Adder(10, 46); // 56
+
+            Console.WriteLine($"these are my results: {result}. Result 2: {result2}");
+
+        }
+
+        static int Adder(int a, int b)
+        {
+            return a + b;
+        }
+
+
+
+    }
+}
